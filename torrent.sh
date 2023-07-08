c4a06934d57e5923856d40f99f9d69a9e9265cd3
raoul <raoul.hernandez1@gmail.com>
2023-07-08 05:56:27 +0000

    fix indentation

--- torrent.sh
+++ torrent.sh
@@ -52,50 +52,50 @@ do
     mkdir -p "$folder"
   fi
 
-echo "[$current/$total] [INFO] $filename"
+  echo "[$current/$total] [INFO] $filename"
 
-find=$(find $movies -iwholename "$filename")
+  find=$(find $movies -iwholename "$filename")
 
-if [[ -n $find ]]; then
-	echo "[$current/$total] [INFO] Found $find"
-  movie="$find"
+  if [[ -n $find ]]; then
+    echo "[$current/$total] [INFO] Found $find"
+    movie="$find"
     if [[ -n "$folder" ]]; then
       ln -s "$movie" "$folder/$filename" 
     else
- 	    ln -s "$movie" "$filename"
+      ln -s "$movie" "$filename"
     fi
-else
-  echo "[$current/$total] [INFO] No match found found for $filename"
-  shortname=$(echo "$filename" | sed 's/\(.\{5\}\).*/\1/g')
-  readarray -d '' array < <(find $movies -iname "$shortname*.mkv" -print0)
-  if [[ ${#array[@]} == 0 ]]; then
-	  shortname=$(echo "$filename" | sed 's/\(.\{2\}\).*/\1/g')
+  else
+    echo "[$current/$total] [INFO] No match found found for $filename"
+    shortname=$(echo "$filename" | sed 's/\(.\{5\}\).*/\1/g')
     readarray -d '' array < <(find $movies -iname "$shortname*.mkv" -print0)
-    # echo "[$current/$total] [ERRO] No match found for $filename"
-  fi
-
-  if [[ ${#array[@]} == 1 ]]; then
-    echo "[$current/$total]" Found "${array[0]}"
-    movie="${array[0]}" 
-    if [[ -n "$folder" ]]; then
-      ln -s "$movie" "$folder/$filename" 
-    else
- 	    ln -s "$movie" "$filename"
+    if [[ ${#array[@]} == 0 ]]; then
+      shortname=$(echo "$filename" | sed 's/\(.\{2\}\).*/\1/g')
+      readarray -d '' array < <(find $movies -iname "$shortname*.mkv" -print0)
+      # echo "[$current/$total] [ERRO] No match found for $filename"
     fi
-  fi
 
-  if [[ ${#array[@]} -gt 1 ]]; then
-		select movie in "${array[@]}"
-		do
-		  echo "[INFO] You have chosen $movie"
+    if [[ ${#array[@]} == 1 ]]; then
+      echo "[$current/$total]" Found "${array[0]}"
+      movie="${array[0]}" 
       if [[ -n "$folder" ]]; then
         ln -s "$movie" "$folder/$filename" 
       else
-		    ln -s "$movie" "$filename"
+        ln -s "$movie" "$filename"
       fi
-		break 
-		done
-	fi
-fi
-current=$((current+1))
+    fi
+
+    if [[ ${#array[@]} -gt 1 ]]; then
+      select movie in "${array[@]}"
+      do
+        echo "[INFO] You have chosen $movie"
+        if [[ -n "$folder" ]]; then
+          ln -s "$movie" "$folder/$filename" 
+        else
+          ln -s "$movie" "$filename"
+        fi
+        break 
+      done
+    fi
+  fi
+  current=$((current+1))
 done
